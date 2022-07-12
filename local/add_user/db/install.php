ad182b4e080ed097903cca74f9907696858631fc
Paweł Kali <pkalisz451@gmail.com>
2022-07-12 18:11:35 +0200

    wstpenie poprawnie dzilajacy install.php todo transformacja dwoch pol na klucze w db

--- local/add_user/db/install.php
+++ local/add_user/db/install.php
@@ -4,11 +4,11 @@
  * @package  moodle_local_plugin
  * @copyright 2017, Mohammed Essaid MEZERREG <me@mohessaid.com>
  * @license MIT
- * @doc 
+ * @doc
  */
 
 // Allows you to execute a PHP code right after the plugin's database scheme has been installed.
- 
+
 defined('MOODLE_INTERNAL') || die();
 
 
@@ -18,37 +18,48 @@ defined('MOODLE_INTERNAL') || die();
  * @throws ddl_exception
  */
 function xmldb_local_add_user_install()
-{
+{//https://docs.moodle.org/dev/XMLDB_creating_new_DDL_functions
     global $DB;
 
     $dbman = $DB->get_manager();
-    
+
     $table = new xmldb_table('user');
-    $position_id_field = new xmldb_field('position_id'); 
+    $position_id_field = new xmldb_field('position_id', XMLDB_TYPE_INTEGER, '10');
 
     if (!$dbman->field_exists($table, $position_id_field)) {
-        $table->add_field('position_id', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, XMLDB_SEQUENCE, null, null);
-        $table->add_key('position_id', XMLDB_KEY_FOREIGN, array('position_id'), 'position', array('id'));
 
-    }
-    $table = new xmldb_table('user');
-    $organizational_unit_id_field = new xmldb_field('organizational_unit_id');
-    
-    if (!$dbman->field_exists($table, $organizational_unit_id_field)) {
+        $dbman->add_field($table, $position_id_field);
        
-        $table->add_field('organizational_unit_id', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, XMLDB_SEQUENCE, null, null);
-        $table->add_key('organizational_unit_id', XMLDB_KEY_FOREIGN, array('organizational_unit_id'), 'organizational_unit', array('id'));
+       // $table->add_key('position_id', XMLDB_KEY_FOREIGN, array('position_id'), 'position', array('id'));
+
+    }
+    $table = new xmldb_table('user');
+    $organizational_unit_id_field = new xmldb_field('organizational_unit_id', XMLDB_TYPE_INTEGER, '10');
+
+    if (!$dbman->field_exists($table, $organizational_unit_id_field)) {
+
+        $dbman->add_field($table, $organizational_unit_id_field);
+        //$table->add_key('organizational_unit_id', XMLDB_KEY_FOREIGN, array('organizational_unit_id'), 'organizational_unit', array('id'));
     }
 
 
     $table = new xmldb_table('user');
-    $employee_number = new xmldb_field('employee_number');
+    $employee_number = new xmldb_field('employee_number', XMLDB_TYPE_CHAR, '10');
 
-    if (!$dbman->field_exists($table,  $employee_number)) {
+    if (!$dbman->field_exists($table, $employee_number)) {
+      
+        $dbman->add_field($table, $employee_number);
 
-        $table->add_field('employee_number', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, XMLDB_SEQUENCE, null, null);
-     
     }
-//employee_number
+   // var_dump( $table);
+
+
+   /* $table = new xmldb_table('calendar');
+    $field = new xmldb_field('field_test'); // You'll have to look up the definition to see what other params are needed.
+
+    if (!$dbman->field_exists($table, $field)) {
+        $dbman->add_field($table, $field);
+    }*/
+
 }
 
