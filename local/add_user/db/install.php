a9049ced1dbecbd2a14cf7a49b2351a29aac5027
Paweł Kali <pkalisz451@gmail.com>
2022-07-13 14:45:48 +0200

    add key failed

--- local/add_user/db/install.php
+++ local/add_user/db/install.php
@@ -16,6 +16,7 @@ defined('MOODLE_INTERNAL') || die();
 /**
  * @throws ddl_table_missing_exception
  * @throws ddl_exception
+ * @throws coding_exception
  */
 function xmldb_local_add_user_install()
 {//https://docs.moodle.org/dev/XMLDB_creating_new_DDL_functions
@@ -29,22 +30,26 @@ function xmldb_local_add_user_install()
     if (!$dbman->field_exists($table, $position_id_field)) {
 
         $dbman->add_field($table, $position_id_field);
-        $position_key = new xmldb_key('position_id', XMLDB_KEY_FOREIGN, array('position_id'), 'position', array('id'));
-        $dbman->add_key($table, $position_key);
 
-      
-        // $table->add_key('position_id', XMLDB_KEY_FOREIGN, array('position_id'), 'position', array('id'));
+        //$key1 = new xmldb_key('foreignkey1');
+       // $key1->set_attributes(XMLDB_KEY_FOREIGN, array('position_id'));
+      //  $dbman->add_key($table, $key1);
 
+
+        $field3 = new xmldb_field('course');
+        $field3->set_attributes(XMLDB_TYPE_INTEGER, '10'); //  [XMLDB_ENUM, null,] Moodle 2.x deprecated
+        $key2 = new xmldb_key('foreignkey1');
+        $key2->set_attributes(XMLDB_KEY_FOREIGN, array('courseid'), 'position', array('id'));
+        //   $table = new xmldb_table('user');
+        $table->addField($field3);
+        $table->addKey($key2);
     }
+
+
     $table = new xmldb_table('user');
     $organizational_unit_id_field = new xmldb_field('organizational_unit_id', XMLDB_TYPE_INTEGER, '10');
 
     if (!$dbman->field_exists($table, $organizational_unit_id_field)) {
-
-        $dbman->add_field($table, $organizational_unit_id_field);
-        $organizational_unit_key = new xmldb_key('organizational_unit_id', XMLDB_KEY_FOREIGN, array('organizational_unit_id'), 'organizational_unit', array('id'));
-        $dbman->add_key($table, $organizational_unit_key);
-
     }
 
     $table = new xmldb_table('user');
