a941554aed03ae4a1c4cdbacf20b8ad7de04a08c
Paweł Kali <pkalisz451@gmail.com>
2022-07-21 10:54:14 +0200

    reinstall adduser

--- local/adduser/db/access.php
+++ local/adduser/db/access.php
@@ -2,7 +2,7 @@
 //https://docs.moodle.org/dev/Access_API
 //okreslanie dostepnosci do db tabel wtyczki za pomoca rol moodle
 $capabilities = [
-    'local/add_user:admin' => [
+    'local/adduser:admin' => [
         'riskbitmask' => RISK_SPAM,
         'captype' => 'write',
         'contextlevel' => CONTEXT_SYSTEM,
