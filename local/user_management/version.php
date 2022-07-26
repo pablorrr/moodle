2ecd307848fc372daa97214979653fd22276fee7
Paweł Kali <pkalisz451@gmail.com>
2022-07-26 14:31:48 +0200

    fix  component name in version.php

--- local/user_management/version.php
+++ local/user_management/version.php
@@ -13,4 +13,4 @@ $plugin->version  = 2022072600;    // The current module version (Date: YYYYMMDD
 $plugin->requires = 2021051100;    // Requires this Moodle version.
 $plugin->maturity = MATURITY_ALPHA; // This is considered as ALPHA for production sites.
 $plugin->release = 'v0.0.1'; // This is our first.
-$plugin->component = 'local_user_managment'; // Declare the type and name of this plugin.
+$plugin->component = 'local_user_management'; // Declare the type and name of this plugin.
