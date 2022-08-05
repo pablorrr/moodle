04f9f5e152f61d56dbdcbb4bfeffcba1a7b976cd
Paweł Kali <pkalisz451@gmail.com>
2022-08-05 13:52:55 +0200

    implement cretevar class to  create page OK

--- local/user_management/create.php
+++ local/user_management/create.php
@@ -30,6 +30,8 @@
 //use add_user\form\form_handle;
 
 
+use local_user_management\createvar;
+
 require_once(__DIR__ . '/../../config.php');//zalacznie moodle
 //require_once(__DIR__ . '\classes\form\form.php');
 require_once($CFG->dirroot . '/user/lib.php');//apply moodle user lib
@@ -66,24 +68,9 @@ $PAGE->set_heading('Create User Page.');
 //
 // ===============
 
-$username = optional_param('username', 'Jake', PARAM_USERNAME);
-$password = optional_param('password', 'Moodle2012!', PARAM_TEXT);
-$idnumber = optional_param('idnumber', 'idnumbertest1', PARAM_TEXT);
-$firstname = optional_param('firstname', 'First Name User Test 1', PARAM_TEXT);
-$lastname = optional_param('lastname', 'Last Name User Test 1', PARAM_TEXT);
-$middlename = optional_param('middlename', 'Middle Name User Test 1', PARAM_TEXT);
-$lastnamephonetic = optional_param('lastnamephonetic', '最後のお名前のテスト一号', PARAM_TEXT);
-$firstnamephonetic = optional_param('firstnamephonetic', '最後のお名前のテスト一号', PARAM_TEXT);
-$alternatename = optional_param('alternatename', 'Alternate Name User Test 1', PARAM_TEXT);
-$email = optional_param('email', 'usertest1@email.com', PARAM_EMAIL);
-$description = optional_param('description', 'This is a description for user 1', PARAM_TEXT);
-$city = optional_param('city', 'Perth', PARAM_TEXT);
-$country = optional_param('country', 'au', PARAM_TEXT);
+$create_vars = new createvar();
 
-$location_vars = ['username', 'password', 'idnumber', 'firstname', 'lastname', 'middlename', 'lastnamephonetic',
-    'firstnamephonetic', 'alternatename', 'email', 'description', 'city', 'country'];
-
-$user = (object)compact($location_vars);
+$user = (object)$create_vars->create_vars();
 
 if (!is_object($user)) {
     return;
